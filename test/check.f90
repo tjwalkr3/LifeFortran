adab99e3f418a432a032ebe6b9d908052075f9dc
Thomas Jones <tjwalkr3@gmail.com>
2024-08-31 19:37:13 -0600

    moved the test into the test file

--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,7 @@
 program check
+use Life, only: print_board
 implicit none
-
-print *, "Put some tests in here!"
+  integer, dimension(3, 3) :: board
+  board = reshape((/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /), shape(board))
+  call print_board(board)
 end program check
