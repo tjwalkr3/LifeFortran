ed440174d1bbf7528e61233d14675e9cb6b2bc84
MateoTomaszeuski <mtomaszeuski@gmail.com>
2024-09-04 16:07:11 -0600

    test pass

--- test/check.f90
+++ test/check.f90
@@ -3,6 +3,7 @@ use Life, only: print_board, count_logic
 implicit none
   integer, dimension(3, 3) :: board1
   integer, dimension(3, 3) :: board2
+  integer, dimension(5, 5) :: board3
 
   ! test the method for printing the board
   print *, "testing print_board"
@@ -16,5 +17,21 @@ implicit none
   print *, "Running on middle cell"
   call count_logic(board2)
   call print_board(board2)
+
+  ! test the method for killing cells in 5 board
+  print *, "test 1 for live_or_die"
+  board3 = reshape((/ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /), shape(board3))
+  call print_board(board3)
+  print *, "Running on middle cell"
+  call count_logic(board3)
+  call print_board(board3)
+
+  ! test the method for killing cells in 5 board
+  print *, "test 1 for live_or_die"
+  board3 = reshape((/ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /), shape(board3))
+  call print_board(board3)
+  print *, "Running on middle cell"
+  call count_logic(board3)
+  call print_board(board3)
   
 end program check
